4f9ef15f3cbc06c3bf7c33769f26c322a2c08339
AppModZone <appmodzone@gmail.com>
2024-12-28 11:42:58 -0500

    Initial commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,3 @@
-window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-W9ZGG6SKDY');
-
 document.addEventListener('DOMContentLoaded', () => {
     const appList = document.getElementById('app-list');
     const categoryList = document.getElementById('category-list');
